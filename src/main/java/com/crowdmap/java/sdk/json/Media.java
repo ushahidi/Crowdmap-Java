aa78ba0b812ebe2290d4b60ed606225a1027a8ec
Henry Addo <addhen@linuxmail.org>
2013-02-16 00:40:56 +0000

    Add media models

---
+++ src/main/java/com/crowdmap/java/sdk/json/Media.java
@@ -0,0 +1,36 @@
+/*****************************************************************************
+ ** Copyright (c) 2010 - 2012 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
+package com.crowdmap.java.sdk.json;
+
+import com.crowdmap.java.sdk.model.Person;
+
+
+/**
+ * Media JSON structure
+ */
+public class Media extends com.crowdmap.java.sdk.model.Media{
+
+	/**
+	 * Serial version UID
+	 */
+	private static final long serialVersionUID = 8498886833164100022L;
+	
+	Person users;
+}
