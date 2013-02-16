a32bb3cdfbe40e5dd8c09adf2a98142da59217a7
Henry Addo <addhen@linuxmail.org>
2013-02-16 01:17:32 +0000

    Add externals service

---
+++ src/main/java/com/crowdmap/java/sdk/service/ExternalsService.java
@@ -0,0 +1,78 @@
+/*****************************************************************************
+ ** Copyright (c) 2010 - 2012 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
+package com.crowdmap.java.sdk.service;
+
+import static com.crowdmap.java.sdk.net.ICrowdmapConstants.SEGMENT_EXTERNALS;
+
+import com.crowdmap.java.sdk.json.ExternalsJson;
+import com.crowdmap.java.sdk.net.CrowdmapHttpClient;
+
+/**
+ * Externals service
+ */
+public class ExternalsService extends BaseService {
+
+	/**
+	 * Create a new external service with the default crowdmap HTTP client
+	 */
+	public ExternalsService() {
+		super();
+	}
+
+	/**
+	 * Create a new externals service with a configured crowdmap HTTP client
+	 * 
+	 * @param client
+	 */
+	public ExternalsService(CrowdmapHttpClient client) {
+		super(client);
+	}
+
+	/**
+	 * Get all externals. GET /externals
+	 * 
+	 * @return 
+	 */
+	public ExternalsJson getExternals() {
+
+		StringBuilder url = new StringBuilder(apiUrl);
+		url.append(SEGMENT_EXTERNALS);
+
+		return fromString(client.sendGetRequest(url.toString()),
+				ExternalsJson.class);
+	}
+
+	/**
+	 * Get specific external. GET /externals/:external_id
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public ExternalsJson getExternal(String id) {
+		StringBuilder url = new StringBuilder(apiUrl);
+		url.append(SEGMENT_EXTERNALS);
+		url.append("/");
+		url.append(id);
+
+		return fromString(client.sendGetRequest(url.toString()),
+				ExternalsJson.class);
+
+	}
+}
