85c95692fb98cc931efb5a43e55b2c7cb66215d5
Henry Addo <addhen@linuxmail.org>
2013-02-25 11:07:49 +0000

    Add permissions model class

---
+++ src/main/java/com/crowdmap/java/sdk/model/Permissions.java
@@ -0,0 +1,60 @@
+/*****************************************************************************
+ ** Copyright (c) 2010 - 2012 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
+package com.crowdmap.java.sdk.model;
+
+/**
+ * Permissions model classes
+ */
+public class Permissions {
+
+	private boolean delete;
+
+	private boolean edit;
+
+	/**
+	 * @return the delete
+	 */
+	public boolean isDelete() {
+		return delete;
+	}
+
+	/**
+	 * @param delete
+	 *            the delete to set
+	 */
+	public void setDelete(boolean delete) {
+		this.delete = delete;
+	}
+
+	/**
+	 * @return the edit
+	 */
+	public boolean isEdit() {
+		return edit;
+	}
+
+	/**
+	 * @param edit
+	 *            the edit to set
+	 */
+	public void setEdit(boolean edit) {
+		this.edit = edit;
+	}
+}
