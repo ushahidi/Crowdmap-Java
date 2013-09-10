22990037f4440841238e4a3b14c0378e8c362a94
Henry Addo <addhen@linuxmail.org>
2013-09-10 12:09:29 +0900

    Upgrade bundled gradle to the latest

--- src/test/java/com/crowdmap/java/sdk/service/BaseServiceTest.java
+++ src/test/java/com/crowdmap/java/sdk/service/BaseServiceTest.java
@@ -28,7 +28,6 @@ import java.util.Collection;
 /**
  * The base class for testing all the task supported by the Ushahidi API
  *
- * @author eyedol
  */
 public abstract class BaseServiceTest extends TestCase {
 
